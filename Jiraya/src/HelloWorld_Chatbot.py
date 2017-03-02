9c690626320fc247fea5f570f332d2e5f4dbb2bd
Kumar Govind Jha <Kumar Govind Jha>
2017-03-03 02:09:23 +0530

    lib upload

--- Jiraya/src/HelloWorld_Chatbot.py
+++ Jiraya/src/HelloWorld_Chatbot.py
@@ -1,7 +1,31 @@
-'''
-Created on Mar 3, 2017
+# -*- coding: utf-8 -*-
 
-@author: JHA
-'''
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
-print(1);
+
+chatbot = ChatBot("Jiraya")
+chatbot.set_trainer(ListTrainer)
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "Hi there!",
+    "Hello",
+    "How are you doing?",
+    "I'm doing great.",
+    "Cool!",
+    "^_^"
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome.",
+]
+
+chatbot.set_trainer(ListTrainer)
+chatbot.train(conversation)
+
+response = chatbot.get_response("Hello")
+print(response)
+
+response = chatbot.get_response("I am hungry.")
+print(response)
